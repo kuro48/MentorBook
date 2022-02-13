b49f61dfb962a296ffea99248b5c7642efdd4471
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-02-14 02:12:43 +0900

    メンターブックDone!!

--- MentorBook/ViewController.swift
+++ MentorBook/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mentorArray:[Mentor] = []
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var namelabel: UILabel!
+    @IBOutlet var courselabel: UILabel!
+    
+    var index: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.[
+        mentorArray.append(Mentor(name: "ながた",imageName: "naagta.jpg",course: "iPone"))
+        mentorArray.append(Mentor(name: "りょう",imageName: "ryo.jpg",course: "Unity"))
+        mentorArray.append(Mentor(name: "たいてぃ",imageName: "taithi.jpg",course: "WebS,WebD"))
+        
+        setUI()
     }
-
-
+    
+    func setUI(){
+        imageView.image = mentorArray[index].getImage()
+        namelabel.text = mentorArray[index].name
+        courselabel.text = mentorArray[index].course
+    }
+    
+    @IBAction func mae() {
+        index = index - 1
+        setUI()
+        }
+    
+    @IBAction func tsugi() {
+        index = index + 1
+        setUI()
+        }
+    
 }
 
